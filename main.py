75edf4a1efbb2cb96a0e8259b63c27255ec27007
raphael-khan <raphaelkhan@yahoo.com>
2022-02-19 13:18:40 -0500

    2.19 update

---
+++ main.py
@@ -0,0 +1,11 @@
+from PIL import Image, ImageFilter
+
+img = Image.open('./pokedex/pikachu.jpg')
+filtered_img = img.filter(ImageFilter.CONTOUR)
+convert_img = img.convert('L')
+convert_img.save('pikachu_convert.jpg')
+convert_img.show()
+filtered_img.save('pikachu_contour.jpg')
+
+# print(img.format, img.size, img.mode)
+# print(dir(img))
